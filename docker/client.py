fad431af26f8790c4c37b32b8a9561c660086e91
Marco Paolini <marco@credra.com>
2014-07-13 00:12:02 +0200

    Downgrade default API version

--- docker/client.py
+++ docker/client.py
@@ -32,7 +32,7 @@ from .tls import TLSConfig
 if not six.PY3:
     import websocket
 
-DEFAULT_DOCKER_API_VERSION = '1.12'
+DEFAULT_DOCKER_API_VERSION = '1.10'
 DEFAULT_TIMEOUT_SECONDS = 60
 STREAM_HEADER_SIZE_BYTES = 8
 
